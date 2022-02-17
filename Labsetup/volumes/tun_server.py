d2bf46f9f52d4b99eb55e8b05521f6a565e8a789
ty4045681 <576552241qwj@gmail.com>
2022-02-17 10:25:51 +0000

    main

---
+++ Labsetup/volumes/tun_server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from scapy.all import *
+import fcntl
+import struct
+import os
+import time
+
+TUNSETIFF = 0x400454ca
+IFF_TUN   = 0x0001
+IFF_TAP   = 0x0002
+IFF_NO_PI = 0x1000
+
+# create TUN interface
+tun = os.open("/dev/net/tun", os.O_RDWR)
+ifr = struct.pack('16sH', b'tun%d', IFF_TUN | IFF_NO_PI)
+ifname_bytes  = fcntl.ioctl(tun, TUNSETIFF, ifr)
+
+# Get the interface name
+ifname = ifname_bytes.decode('UTF-8')[:16].strip("\x00")
+print("Interface Name: {}".format(ifname))
+
+os.system("ip addr add 192.168.45.99/24 dev {}".format(ifname))
+os.system("ip link set dev {} up".format(ifname))
+os.system(f"ip route add 192.168.50.0/24 dev {ifname} via 192.168.45.99")
+
+
+IP_A = "0.0.0.0"
+PORT = 9090
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((IP_A, PORT))
+while True:
+   # this will block until at least one interface is ready
+   ready, _, _ = select.select([sock, tun], [], [])
+
+   for fd in ready:
+      if fd is sock:
+         data, (ip, port) = sock.recvfrom(2048)
+         pkt = IP(data)
+         print("From socket <==: {} --> {}".format(pkt.src, pkt.dst))
+         send(pkt)
+      if fd is tun:
+         packet = os.read(tun, 2048)
+         pkt = IP(packet)
+         print("From tun    ==>: {} --> {}".format(pkt.src, pkt.dst))
+         sock.sendto(packet, ('10.9.0.12', 9090))
+   
